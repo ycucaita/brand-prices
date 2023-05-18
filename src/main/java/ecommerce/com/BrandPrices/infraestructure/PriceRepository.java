6858e5a4c8de00946825950cdd657407ae9c7534
ycucaita <eingener8909@gmail.com>
2023-05-18 13:19:12 +0200

    Improve query

--- src/main/java/ecommerce/com/BrandPrices/infraestructure/PriceRepository.java
+++ src/main/java/ecommerce/com/BrandPrices/infraestructure/PriceRepository.java
@@ -12,16 +12,15 @@ import java.util.Optional;
 @Repository
 public interface PriceRepository extends CrudRepository<Price, Integer> {
 
-    @Query(value="SELECT * FROM PRICES\n" +
-            " WHERE START_DATE= ?1 \n" +
-            " AND START_DATE_HOUR<= ?2 \n" +
-            " AND END_DATE_HOUR> ?2 "+
-            " AND PRODUCT_ID = ?3 \n" +
-            " AND BRAND_ID = ?4 \n" +
-            "AND PRIORITY =(SELECT MAX(PRIORITY ) FROM PRICES\n" +
-            "  WHERE START_DATE= ?1 \n" +
-            "  AND START_DATE_HOUR<= ?2 \n" +
-            "  AND END_DATE_HOUR> ?2 );",
+    @Query(value="SELECT *\n" +
+            "FROM PRICES\n" +
+            "WHERE START_DATE = ?1\n" +
+            "  AND START_DATE_HOUR <= ?2\n" +
+            "  AND END_DATE_HOUR > ?2\n" +
+            "  AND PRODUCT_ID = ?3\n" +
+            "  AND BRAND_ID = ?4\n" +
+            "ORDER BY PRIORITY DESC\n" +
+            "LIMIT 1",
             nativeQuery = true)
     Optional<Price> getPrices(LocalDate startDate, LocalTime startHour,
                               int productId, int brandId);
